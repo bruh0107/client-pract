21a957ed67bfd865f1bb33f81c1f3798d6737faa
Nastya <anastasion0107@gmail.com>
2025-03-10 21:29:48 +0700

    instance binding

--- vue/js/main.js
+++ vue/js/main.js
@@ -3,6 +3,9 @@ let description = 'A pair of warm, fuzzy socks'
 let app = new Vue({
     el: '#app',
     data: {
-        product: "Socks"
+        product: "Socks",
+        image: "./assets/vmSocks-green-onWhite.jpg",
+        altText: "A pair of socks",
+        link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks'
     }
 })
