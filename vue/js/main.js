b3365491e7c6b4ccb4472bbf67e33cbae94292be
Nastya <anastasion0107@gmail.com>
2025-03-10 23:23:14 +0700

    list rendering

--- vue/js/main.js
+++ vue/js/main.js
@@ -10,5 +10,17 @@ let app = new Vue({
         inStock: true,
         inventory: 50,
         onSale: true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green',
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue',
+            },
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
     }
 })
